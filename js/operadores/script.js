d24e40372b15f855de3ffa0532a8b8e990ecc29c
Otavio Azevedo <otavioazevedo39@gmail.com>
2025-04-13 17:20:17 -0300

    operadores/strings

---
+++ js/operadores/script.js
@@ -0,0 +1,16 @@
+// operadores em js - typeof number
+console.log(typeof 2)
+console.log(typeof "2")     
+
+// operações aritméticas
+console.log(10 + 7) 
+console.log(10 / 7) 
+console.log(10 * 7) 
+// strings 
+console.log("texto simples")
+console.log(` texto com crase`)
+console.log("texto com aspas simples")
+
+
+
+
