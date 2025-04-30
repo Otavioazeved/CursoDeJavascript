4c90286a1d15b3f02ecd0a11cfdcea841052915c
Otavio Azevedo <otavioazevedo39@gmail.com>
2025-04-30 16:11:54 -0300

    operadores logicos

--- js/operadores/script.js
+++ js/operadores/script.js
@@ -1,15 +1,60 @@
 // operadores em js - typeof number
-console.log(typeof 2)
-console.log(typeof "2")     
+//console.log(typeof 2)
+//console.log(typeof "2")     
 
 // operações aritméticas
-console.log(10 + 7) 
-console.log(10 / 7) 
-console.log(10 * 7) 
+//console.log(10 + 7) 
+//console.log(10 / 7) 
+//console.log(10 * 7) 
 // strings 
-console.log("texto simples")
-console.log(` texto com crase`)
-console.log("texto com aspas simples")
+//console.log("texto simples")
+//console.log(` texto com crase`)
+//console.log("texto com aspas simples")
+
+// simbolos especias em string
+//console.log("testando a \n quebra de linha")
+
+// concatenação
+
+//console.log("oi," + "tudo" + " bem")
+//console.log(`testando`+ `com` + ` crase`)
+
+// interpolação
+//.log(` a soma de 2 + 2 é: ${2 + 2}` )
+
+// boolean 
+
+//console.log(true)
+//console.log(5 > 20)  // nesse caso aqui ele recebe esse valor, mas aparece false
+
+// comparações
+
+//.log(4 <=2)
+//onsole.log(1 != 2) // sinal de diferente !=, nesse caso ele me resulta true, pois 1 é diferente de 2
+//console.log(5 === 5) // sinal de identico ===
+
+// identico
+
+//console.log(9 + "99")   
+
+//console.log(9==9)
+
+//console.log("9" != 99)
+
+// operadores logicos
+// or -> ||  and -> &&    ! -> no, o resul é um boolean
+
+console.log(true && false)
+// aqui vai da false, pois so precisa de um falso no and p dar false
+console.log(true && true)
+console.log(true || false)
+
+console.log(!true)
+// aqui vai da false, msm tendo o true ali, pois o sinal de exclamação ele inverte o resultado.
+
+
+
+
 
 
 
