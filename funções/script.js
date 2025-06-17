25dc2bf29746be7c9d9744dea8ef758bed0da3a4
Otavio Azevedo <otavioazevedo39@gmail.com>
2025-06-17 12:46:48 -0300

    iniciando estudos das funções em js

---
+++ funções/script.js
@@ -0,0 +1,27 @@
+// criando uma função
+function somar(a, b){
+    return a + b;
+}
+
+console.log(somar(1, 4));
+
+const minhaFuncaoEmVariavel = function(){
+    console.log("Função em variavel");
+}
+minhaFuncaoEmVariavel();
+
+// return 
+
+const a = 10;
+const b = 20;
+
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+const resultado = soma(a, b);
+
+console.log(resultado);
+
+
+
